9097e430705d2119b8df33a402fe681a0d19354b
Pavle Marinkovic <payche255@gmail.com>
2023-11-22 16:42:33 +0100

    first-commit

---
+++ Loop I/domaci.php
@@ -0,0 +1,30 @@
+<?php
+
+// Zadatak 1
+
+$ime = 'administrator';
+$lozinka = 'mojaSifraJeSigurna';
+
+$trazenoIme = 'adminiSTRATOR'; // test Ime
+
+
+if (strcasecmp($ime, $trazenoIme) == 0 && $lozinka) {
+    echo "Dobrodošao $ime";
+} else {
+    echo 'Vase ime ili lozinka su pogrešni.';
+}
+
+echo '<br>';
+
+// Zadatak 2
+
+// Rucno postavljeno vreme (primer)
+$sati = 20; // Ovde možete postaviti bilo koje vreme koje želite proveriti
+
+if ($sati >= 5 && $sati < 12) {
+    echo "Jutro";
+} elseif ($sati >= 12 && $sati < 20) {
+    echo "Podne";
+} elseif ($sati >= 20 || $sati < 5) {
+    echo "Noć";
+}
---
+++ Loop I/readme.md
@@ -0,0 +1,3 @@
+### Pitanje:
+ - Da li koristiti "strcasecmp" ili sve stringove mozda prebaciti u "lowercase" i tako onda proveratati ?
+ - Nisam znao za ovu funkciju "strcasecmp", izguglao sam i tako dobio resenje, ali moja prva logika je bila ici sa "strtolower"
